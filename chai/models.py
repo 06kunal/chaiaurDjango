18a720ddb7cce373703164ee3b3a092b288165d6
Kunal Garg <kt.garg437@gmail.com>
2026-01-23 23:54:03 +0530

    added more models

--- chai/models.py
+++ chai/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class ChaiVarity(models.Model):
+    CHAI_TYPE_CHOICE = [
+        ('ML', 'MASALA'),
+        ('GR', 'GINGER'),
+        ('KL', 'KIWI'),
+        ('PL', 'PLAIN'),
+        ('EL', 'ELACHI'), 
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='chais/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices= CHAI_TYPE_CHOICE)
+    description = models.TextField(default='')
+    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Chai Price')
+
+    def __str__(self):
+        return self.name
+
+#Relationships
+#One to Many
+class ChaiReview(models.Model):
+    chai = models.ForeignKey(ChaiVarity, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    RATING_TYPE = [
+        (1, 'Very Bad'),
+        (2, 'Bad'),
+        (3, 'Neutal'),
+        (4, 'Good'),
+        (5, 'Very Good'),
+    ]
+    rating = models.IntegerField(choices=RATING_TYPE)
+    comment = models.TextField(default='')
+    date_added = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return f'{self.user.username} review for {self.chai.name}'
+    
+#Many to Many
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    chai_varieties = models.ManyToManyField(ChaiVarity, related_name='stores')
+
+    def __str__(self):
+        return self.name
+    
+#One to One
+class chaiCertificate(models.Model):
+    chai = models.OneToOneField(ChaiVarity, on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=100)
+    issued_date = models.DateTimeField(default=timezone.now)
+    valid_until = models.DateTimeField()
+
+    def __str__(self):
+        return f'Certificate for {self.name.chai}'
+
+    
+
+
